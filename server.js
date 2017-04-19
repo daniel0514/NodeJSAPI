8b18e9b813dee998d78be5cec321fd165f45feb5
daniel0514 <daniel0514@gmail.com>
2017-04-19 16:37:12 -0700

    changed API and changed ReactJS to AngularJS in AboutMe Description

--- server.js
+++ server.js
@@ -1,45 +1,67 @@
 var express = require('express');
 var app = express();
 var mongo = require('mongodb');
+var mongoClient = mongo.MongoClient;
 
 app.use(express.static(__dirname + "/public"));
-
 app.get('/projects', function(req, res){
-    if(projects.length < 2){
-        res.status(404);
-    } else {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.json(projects);
-    }
-});
-
-
-var mongoClient = mongo.MongoClient;
-var projects;
-mongoClient.connect("mongodb://localhost:27017/Portfolio", function(err, db){
-   if(!err){
-       console.log("Connected to DB");
-       db.collection('Projects', function(err, collection){
-          if(!err){
-              console.log("Project Collection found");
-                collection.find().toArray(function(err, items) {
-                    if(err){
-                        console.log("Error: " + error);
-                    } else {
-                        if(items.length < 1) {
-                            console.log("No Items in Collection");
+    mongoClient.connect("mongodb://localhost:27017/Portfolio", function(err, db){
+        if(!err){
+            console.log("Request Received and Connected to DB");
+            db.collection('Projects', function(err, collection){
+                if(!err){
+                    console.log("Project Collection found");
+                    collection.find().toArray(function(err, items) {
+                        if(err){
+                            res.status(500)
+                                .send(err);
+                            console.log("Error: " + error);
                         } else {
-                            console.log("One or more items found");
-                            projects = items;
+                            res.header("Access-Control-Allow-Origin", "*")
+                                .json(items);
                         }
-                    }
-                })
-          } else {
-              console.log("Unable to find Projects Collection");
-          }
-       });
-   }
+                    })
+                } else {
+                    res.status(500)
+                        .send(err);
+                    console.log("Error: " + error);
+                }
+            });
+        }
+    });
 });
+app.get('/projects/:projectName', function(req, res){
+    mongoClient.connect("mongodb://localhost:27017/Portfolio", function(err, db){
+        if(!err){
+            console.log("Request Received and Connected to DB");
+            db.collection('Projects', function(err, collection){
+                if(!err){
+                    console.log("Project Collection found");
+                    collection.findOne({ name: req.param('projectName')}, function(err, document){
+                        if(!err){
+                            if(document != null){
+                                res.header("Access-Control-Allow-Origin", "*")
+                                    .json(document);
+                            } else {
+                                res.status(404)
+                                    .send("No Project found");
+                            }
+                        } else {
+                            res.status(500)
+                                .send(err);
+                            console.log("Error: " + error);
+                        }
+                    });
+                } else {
+                    res.status(500)
+                        .send(err);
+                    console.log("Error: " + error);
+                }
+            });
+        }
+    });
+});
+
 
 app.listen(8000);
 console.log('API is now running on port 8000');
